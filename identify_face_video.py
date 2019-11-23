8f31654e5dac0044877e4840c7b7c41c19d2f7d6
Harsath - Panther <harsathpanther@gmail.com>
2019-11-23 12:49:00 +0530

    Update identify_face_video.py

--- identify_face_video.py
+++ identify_face_video.py
@@ -1,3 +1,6 @@
+# This Script Author: Harsath KT
+# Other Tensorflow Paper Implementation: Dr.David
+
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
